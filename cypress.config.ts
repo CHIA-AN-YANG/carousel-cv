1b13a7a997cc5397a6c5ae46b81b25c29ad0949d
Anna Yang <anna.yang@seidor.com>
2025-02-21 17:25:39 +0800

    change ui view

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    pageLoadTimeout: 120000,
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // Implement event listeners if needed
