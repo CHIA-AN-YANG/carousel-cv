e26b42a214c4e818458de4efd83a6b7b3ec99491
Anna Yang <anna.yang@seidor.com>
2025-02-21 13:55:54 +0800

    increase load timeout

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    pageLoadTimeout: 120000,
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // Implement event listeners if needed
